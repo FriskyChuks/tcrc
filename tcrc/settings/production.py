36e4a794ac8034adb2254305f6d99f1d68cfb7cd
unknown <chuksedwin.ce@gmail.com>
2022-09-09 12:45:39 +0100

    allowed hosts updated

--- tcrc/settings/production.py
+++ tcrc/settings/production.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-d2n995k6e(9=y$#xbaa(l%otbbpns7zskyz$4!*xvy&mg*l#7i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tcrc.herokuapp.com']
 
 
 # Application definition
