30699d509b71acda473902a3c33684c0f247fd54
unknown <chuksedwin.ce@gmail.com>
2022-09-09 12:50:29 +0100

    production.py updated

--- tcrc/settings/production.py
+++ tcrc/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d2n995k6e(9=y$#xbaa(l%otbbpns7zskyz$4!*xvy&mg*l#7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tcrc.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
