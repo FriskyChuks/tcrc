e7fa34d3512b29614051ace6369c8c5aae7331a7
unknown <chuksedwin.ce@gmail.com>
2022-09-09 11:41:31 +0100

    debug=True

--- tcrc/settings/production.py
+++ tcrc/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d2n995k6e(9=y$#xbaa(l%otbbpns7zskyz$4!*xvy&mg*l#7i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
